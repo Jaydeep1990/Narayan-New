656ce2e2576c6d290ed5015b6cc8317ba5e19746
jaydeep chavan <jaydeep111chavan@gmail.com>
2022-05-31 10:29:00 +0530

    pratap is removed

--- Array/src/array/Array2.java
+++ Array/src/array/Array2.java
@@ -4,8 +4,7 @@ public class Array2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//jaydeep chavan pratap
-//jaydeep chavna going to the home
+
  /* String name="Jaydeep";
    String name1="Jaydeel";
   System.out.println(name.compareTo(name1));
